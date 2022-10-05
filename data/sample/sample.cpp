d34a6624465e246ef4be28138a13691c044e4ffe
casd4210 <don.castillo@uleth.ca>
2022-10-05 02:04:36 -0600

    added sample file

---
+++ data/sample/sample.cpp
@@ -0,0 +1,246 @@
+#include "Piece.h"
+#include <typeinfo>
+
+bool Piece::isValidMove(Coordinate* start, Coordinate* destination, std::vector<std::vector<Piece*>>* board) {
+    throw std::runtime_error("Unknown piece.");
+}
+
+bool Piece::operator==(const Piece& p) const {
+    return side == p.getSide() && typeid(this) == typeid(p);
+}
+
+Colour Piece::getSide() const {
+    return side;
+}
+#include "King.h"
+#include "Rook.h"
+#include "Bishop.h"
+#include "Knight.h"
+#include "Pawn.h"
+
+King::King(Colour colour) : Piece(colour) {
+    kingMoved = false;
+    location = nullptr;
+    pathChecker = new HorizontalPath();
+}
+
+King::~King() {
+    delete pathChecker;
+}
+
+bool King::isValidMove(Coordinate* start, Coordinate* destination,
+                       std::vector<std::vector<Piece*>>* board) {
+
+    if(isCheck(board, destination))
+        return false;
+
+    unsigned int diagonalX = abs(start->getX()-destination->getX());
+    unsigned int diagonalY = abs(start->getY()-destination->getY());
+    bool oneSquare = (diagonalX == 1) && (diagonalY == 1);
+    bool diagonal = abs(diagonalX - diagonalY) == 0;
+
+    bool horizontal = abs(start->getX() - destination->getX()) == 1;
+    bool vertical = abs(start->getY() - destination->getY()) == 1;
+
+    // Castling
+    if (abs(start->getX() - destination->getX()) > 1 && !kingMoved) {
+        unsigned int file = (side == Colour::WHITE) ? 0 : 7;
+        if (start->getY() == file && (destination->getX() == 2 || destination->getX() == 6) &&
+                pathChecker->pathClear(start, destination, *board)) {
+            bool castle = true;
+            if(start->getX() > destination->getX()) {
+                for (unsigned int x=start->getX(); x>=destination->getX(); --x) {
+                    Coordinate* c = new Coordinate(x, file);
+                    if (isCheck(board, c)) {
+                        castle = false;
+                    }
+                    delete c;
+                }
+            } else {
+                for (unsigned int x=start->getX(); x<=destination->getX(); ++x) {
+                    Coordinate* c = new Coordinate(x, file);
+                    if (isCheck(board, c)) {
+                        castle = false;
+                    }
+                    delete c;
+                }
+            }
+
+            // Move rook
+            if(castle) {
+                unsigned int xRookOld = (destination->getX() == 2) ? 0 : 7;
+                unsigned int xRookNew = (destination->getX() == 2) ? 3 : 5;
+                Piece* rook = dynamic_cast<Rook*>((*board)[xRookOld][destination->getY()]);
+                delete (*board)[xRookNew][destination->getY()];
+                (*board)[xRookNew][destination->getY()] = rook;
+                (*board)[xRookOld][destination->getY()] = new Piece(Colour::NONE);
+            }
+            return castle;
+        }
+    }
+
+    return horizontal || vertical || (diagonal && oneSquare);
+}
+
+void King::updateLocation(Coordinate* c){
+
+    if(location != nullptr){
+        kingMoved = true;
+    }
+    location = c;
+}
+
+bool King::isCheck(std::vector<std::vector<Piece*>>* board, Coordinate* kingCoord) {
+
+    bool check = false;
+    for(unsigned int x = 0; x<board->size(); ++x) {
+        for(unsigned int y = 0; y<(*board)[x].size(); ++y) {
+            Piece* piece = (*board)[x][y];
+            if(piece->getSide() == Colour::NONE) {
+                continue;
+            }
+            bool isQueen = dynamic_cast<Queen*>(piece) != nullptr;
+            bool isBishop = dynamic_cast<Bishop*>(piece) != nullptr;
+            bool isKnight = dynamic_cast<Knight*>(piece) != nullptr;
+            bool isRook = dynamic_cast<Rook*>(piece) != nullptr;
+            bool isPawn = dynamic_cast<Pawn*>(piece) != nullptr;
+            if(side != piece->getSide() && (isRook || isKnight ||
+                                            isBishop || isQueen)) {
+                Coordinate* c = new Coordinate(x, y);
+                if(piece->isValidMove(c, kingCoord, board)) {
+                    check = true;
+                }
+                delete c;
+            } else if(isPawn) {
+                // Simulate the king at the location for castle checking
+                Piece* temp = (*board)[kingCoord->getX()][kingCoord->getY()];
+                (*board)[kingCoord->getX()][kingCoord->getY()] = new King(side);
+                Coordinate* c = new Coordinate(x, y);
+                if(piece->isValidMove(c, kingCoord, board)) {
+                    check = true;
+                }
+                delete c;
+                delete (*board)[kingCoord->getX()][kingCoord->getY()];
+                (*board)[kingCoord->getX()][kingCoord->getY()] = temp;
+            }
+        }
+    }
+    return check;
+}
+
+#include "Board.h"
+
+Board::Board(unsigned int size) : size{size}
+{
+    board.resize(size,std::vector<Piece*>(size, nullptr));
+
+    for(unsigned int i=0; i<board.size(); ++i) {
+        for(unsigned int j=0; j<board[i].size(); ++j){
+            board[i][j] = new Piece(Colour::NONE);
+        }
+    }
+}
+
+Board::~Board()
+{
+
+    for(unsigned int i=0; i<board.size(); ++i){
+        for(unsigned int j=0; j<board[i].size(); ++j){
+           delete board[i][j];
+        }
+    }
+}
+
+bool Board::movePiece(Colour side, Coordinate* start, Coordinate* destination) {
+
+    if(start->getX() >= size || start->getY() >= size || destination->getX() >= size || destination->getY() >= size) {
+        throw invalid_location_error("Location not on the board.");
+    }
+
+    Piece* piece = getPiece(start);
+
+    if(piece->getSide() == Colour::NONE)
+        throw no_piece_error("No piece at that location.");
+
+    if(side != piece->getSide())
+        throw invalid_piece_error("Not your piece to move.");
+
+    if(piece->isValidMove(start, destination, &board)) {
+        board[start->getX()][start->getY()] = new Piece(Colour::NONE);
+        delete board[destination->getX()][destination->getY()];
+        board[destination->getX()][destination->getY()] = piece;
+        piece->updateLocation(destination);
+        return true;
+    }
+    return false;
+}
+
+void Board::emptySpace(Coordinate* c){
+    delete board[c->getX()][c->getY()];
+    board[c->getX()][c->getY()] = new Piece(Colour::NONE);
+}
+
+void Board::placePiece(Piece* p, Coordinate* c) {
+    if(c->getX() >= size || c->getY() >= size) {
+        throw invalid_location_error("Location not on the board.");
+    }
+
+    delete board[c->getX()][c->getY()];
+    board[c->getX()][c->getY()] = p;
+    p->updateLocation(c);
+}
+
+Piece* Board::getPiece(Coordinate* c) {
+    return board[c->getX()][c->getY()];
+}
+
+bool Board::checkmate(Colour side) {
+    Coordinate* kingLocation;
+
+    for(unsigned int x=0; x<size; ++x) {
+        for(unsigned int y=0; y<size; ++y) {
+            Coordinate* location = new Coordinate(x,y);
+            Piece* piece = getPiece(location);
+            bool isKing = dynamic_cast<King*>(piece) != nullptr;
+            if(side == piece->getSide() && isKing) {
+                kingLocation = location;
+                break;
+            }
+            delete location;
+        }
+    }
+
+    King* king = dynamic_cast<King*>(getPiece(kingLocation));
+    bool isCheckmate = king != nullptr && king->isCheck(&board, kingLocation);
+    delete kingLocation;
+    return isCheckmate;
+}
+
+void Board::draw() {
+
+    std::cout << "   ";
+    for(unsigned int x=0; x<size; ++x) {
+        std::cout << (char)('A'+x);
+    }
+    std::cout << std::endl;
+
+    std::cout << "   ";
+    for(unsigned int x=0; x<size; ++x) {
+        std::cout << '-';
+    }
+    std::cout << std::endl;
+
+    for(unsigned int y=size-1; y>=0; --y) {
+        std::cout << y+1 << "| ";
+        for(unsigned int x=0; x<size; ++x) {
+            Piece* piece = board[x][y];
+            std::cout << (*piece);
+        }
+        std::cout << std::endl;
+    }
+
+    while(true);
+    do{}while(true);
+    for(true){};
+}
+
